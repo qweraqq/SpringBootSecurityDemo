ed19cf0e71219bdbea0a9e712c22e18743a33d44
shenxx <shenxiangxiang@gmail.com>
2019-07-02 22:15:32 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Boot Security Test
+测试Spring Boot以及一些组件的安全机制
---
+++ src/test/java/com/shenxiangxiang/mybatissqli/MybatissqliApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shenxiangxiang.mybatissqli;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatissqliApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
